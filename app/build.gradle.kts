d78aa8a39c576bf83d1716464eb56b7e17581018
maxi <arora.anubhav93@gmail.com>
2026-03-20 15:20:19 +0530

    Add ImageLoader module in the main app build gradle file

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,6 +36,8 @@ android {
 }
 
 dependencies {
+    implementation(project(":core:imageloader"))
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
